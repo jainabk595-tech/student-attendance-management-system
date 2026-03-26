1f56ada9a5f845f473f8e540f26665accbd536f7
Jainab khatoon <jainabk595@gmail.com>
2026-03-26 16:27:56 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <p>© 2026 Mehendi Booking | All Rights Reserved</p>
+</footer>
+
+<!-- JAVASCRIPT LINK -->
+<script src="/mehendi_booking/assets/js/script.js"></script>
+
+</body>
+</html>
