1e94f118255af723db15b2185a0cf37c4564de66
Jainab khatoon <jainabk595@gmail.com>
2026-03-26 16:28:14 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Mehendi Booking</title>
+
+<link rel="stylesheet" href="/mehendi_booking/assets/css/style.css">
+
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+<header class="navbar">
+    <h2 class="logo">Mehendi</h2>
+
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">Location</a>
+        <a href="#">Date</a>
+        <a href="#">Service Type</a>
+    </nav>
+
+    <button class="login-btn">Sign In</button>
+</header>
